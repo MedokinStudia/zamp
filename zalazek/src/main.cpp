c76c0542936aa5ca8e77d313b6dbcba994c63cb7
MedokinStudia <102384600+MedokinStudia@users.noreply.github.com>
2022-11-29 20:47:50 +0100

    Add files via upload

--- zalazek/src/main.cpp
+++ zalazek/src/main.cpp
@@ -3,6 +3,14 @@
 #include <cassert>
 #include <cstdio>
 #include <sstream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <vector>
 #include "../inc/Interp4Command.hh"
 #include "../inc/MobileObj.hh"
 #include "../inc/LibInterface.hh"
@@ -11,12 +19,55 @@
 #include "../inc/xmlinterp.hh"
 #include "../inc/AccessControl.hh"
 #include "../inc/Port.hh"
-
+//#include "../inc/klient.hh"
 
 
 #define LINE_SIZE 500
 
 using namespace std;
+
+
+int Send(int Sk2Server, const char *sMesg)
+{
+  ssize_t  IlWyslanych;
+  ssize_t  IlDoWyslania = (ssize_t) strlen(sMesg);
+
+  while ((IlWyslanych = write(Sk2Server,sMesg,IlDoWyslania)) > 0) {
+    IlDoWyslania -= IlWyslanych;
+    sMesg += IlWyslanych;
+  }
+  if (IlWyslanych < 0) {
+    cerr << "*** Blad przeslania napisu." << endl;
+  }
+  return 0;
+}
+
+
+bool OpenConnection(int &rSocket)
+{
+  struct sockaddr_in  DaneAdSerw;
+
+  bzero((char *)&DaneAdSerw,sizeof(DaneAdSerw));
+
+  DaneAdSerw.sin_family = AF_INET;
+  DaneAdSerw.sin_addr.s_addr = inet_addr("127.0.0.1");
+  DaneAdSerw.sin_port = htons(PORT);
+
+
+  rSocket = socket(AF_INET,SOCK_STREAM,0);
+
+  if (rSocket < 0) {
+     cerr << "*** Blad otwarcia gniazda." << endl;
+     return false;
+  }
+
+  if (connect(rSocket,(struct sockaddr*)&DaneAdSerw,sizeof(DaneAdSerw)) < 0)
+   {
+     cerr << "*** Brak mozliwosci polaczenia do portu: " << PORT << endl;
+     return false;
+   }
+  return true;
+}
 
 int main()
 {
@@ -36,6 +87,29 @@ int main()
   Scene               Scn;
   int                 Socket4Sending;
   
+  if (!OpenConnection(Socket4Sending))
+    return 1;
+  
 
+
+  //Sender   ClientSender(Socket4Sending,&Scn);
+
+  const char *sConfigCmds =
+  "Clear\n"
+  "AddObj Name=Podstawa1 RGB=(20,200,200) Scale=(4,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,20) Trans_m=(-1,3,0)\n"
+  "AddObj Name=Podstawa1.Ramie1 RGB=(200,0,0) Scale=(3,3,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(4,0,0)\n"
+  "AddObj Name=Podstawa1.Ramie1.Ramie2 RGB=(100,200,0) Scale=(2,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(3,0,0)\n"       
+  "AddObj Name=Podstawa2 RGB=(20,200,200) Scale=(4,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(-1,-3,0)\n"
+  "AddObj Name=Podstawa2.Ramie1 RGB=(200,0,0) Scale=(3,3,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(4,0,0)\n"
+  "AddObj Name=Podstawa2.Ramie1.Ramie2 RGB=(100,200,0) Scale=(2,2,1) Shift=(0.5,0,0) RotXYZ_deg=(0,-45,0) Trans_m=(3,0,0)\n";
+
+  cout << "Konfiguracja:" << endl;
+  cout << sConfigCmds << endl;
+
+  Send(Socket4Sending,sConfigCmds);
+  Send(Socket4Sending,"Close\n");
+
+  //Send(Socket4Sending, LibraryList -> getScene());
+  //Send(Socket4Sending,"Close\n");
   return 0;
 }
